353bfacc8154b9beb1d1865d49bcde0cf2fc9339
FirstThinker_Swapnil <swapnilsinha81@gmail.com>
2023-05-19 22:42:21 +0530

    Minor Changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -36,23 +36,66 @@
 
 
 import { useState } from "react";
-import {employees} from './employees';
+import {employees} from '../assets/employees';
 import './SearchBar.css';
+import {FaSearch} from "react-icons/fa";
+
+// function SearchBar(){
+//     const [query,setQuery] =  useState("");
+//     console.log(employees.filter(employees=>employees.name.toLowercase().includes("fe")));
+//     return(
+//         <div className="app">
+//             <input type="text" placeholder="Type to search... " className="search" onChange={e=>setQuery(e.target.value)}/>
+//             <ul className="list">
+//                 {employees.filter((employees)=>employees.name.toLowerCase().includes(query)).map((employees)=>(
+//                     <li key = {employees.id} className="listItem">{employees.name}</li>
+//                 ))}
+//             </ul>
+
+//         </div>
+//     )
+// }
 
 function SearchBar(){
-    const [query,setQuery] =  useState("");
-    // console.log(employees.filter(employees=>employees.name.toLowercase().includes("fe")));
+
+    const [query,setQuery] = useState("");
+    // console.log(employees.filter(employees=>employees.name.includes("f")));
+    // console.log(query);
+    // console.log(employees.filter(user=>user.name.includes("f")));
+
+    console.log(query);
     return(
         <div className="app">
-            <input type="text" placeholder="Type to search... " className="search" onChange={e=>setQuery(e.target.value)}/>
+            
+            <input 
+                type="text" 
+                placeholder="Type to search..." 
+                className="search" 
+                onChange={(e) => setQuery(e.target.value)}
+            />
+
+            <FaSearch id="search-icon" />
+            {/* <ul className="list">
+                {employees.filter((employees)=>
+                employees.name.toLowerCase().includes(query)
+                ).map((employees)=>(
+                    <li key={employees.id} className="listItem">
+                        {employees.name}
+                    </li>
+                ))}
+            </ul> */}
             <ul className="list">
-                {employees.filter(employees=>employees.name.toLowerCase().includes(query)).map((employees)=>(
-                    <li key = {employees.id} className="listItem">{employees.name}</li>
+                {employees.map((user)=>
+                (
+                    // user.name.toLowerCase().includes(query)).map((user)=>(
+                    <li key={user.id} className="listItem">
+                        {user.name}
+                    </li>
                 ))}
             </ul>
 
         </div>
-    )
+    );
 }
 
 export default SearchBar;
